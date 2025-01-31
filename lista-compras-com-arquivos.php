90fbd1a6dd90308e32f32d3a1bad82af8a06deb1
Matheus Monteiro <matheusmnt.dev@gmail.com>
2025-01-30 22:24:02 -0300

    Update lista-compras-com-arquivos.php

--- lista-compras-com-arquivos.php
+++ lista-compras-com-arquivos.php
@@ -1,110 +1,136 @@
-<?php 
- 
-$listadeCompras = []; 
+<?php
 
-function adicionarItem($item) {
-    global $listadeCompras;
-    array_push($listadeCompras, $item);
+$listaCompras = [];
 
-    $arrayConvertido = serialize($listadeCompras);
+function handleFile() {
+	$arquivo = file_get_contents("lista-compras.txt");
 
-    $arquivoSalvo = file_put_contents("lista-de-compras.txt", $arrayConvertido);
+	$arrayListaDeCompras = unserialize($arquivo);
 
-    if ($arquivoSalvo === false) {
-        echo "Falha ao salvar o item na lista!\n";
-        return;
-    } else { 
-        echo "Item adicionado à lista!\n";
-    }
+	if ($arrayListaDeCompras === false) {
+        	return "Erro ao converter a lista de compras";
+    	} 
+
+	if (count($arrayListaDeCompras) == 0) {
+        	return "A lista está vazia, não há itens para remover.\n";
+    	}
+
+	return $arrayListaDeCompras;
 }
 
-function removerItem() {
-    $arquivo = file_get_contents("lista-de-compras.txt");
+function adicionarItem($item) {
+	global $listaCompras;
 
-    $arrayListaDeCompras = unserialize($arquivo); 
+	array_push($listaCompras, $item);
 
-    if ($arrayListaDeCompras === false) {
-        echo "Erro ao converter a lista de compras";
-        return;
-    }  
+	$arquivo = file_get_contents("lista-compras.txt");
 
-    if (count($arrayListaDeCompras) == 0) {
-        echo "A lista está vazia, não há itens para remover.\n";
-        return;
-    }
-    
-    $item = array_pop($arrayListaDeCompras);
+	$arrayListaDeCompras = unserialize($arquivo);
 
-    $arquivoSalvo = file_put_contents("lista-de-compras.txt", serialize($arrayListaDeCompras)); 
+	if (!is_array($arrayListaDeCompras)) {
+		$arrayListaDeCompras = [];
+	}
 
-    if ($arquivoSalvo === false) {
-        echo "Falha ao remover $item da lista!\n";
-        return;
-    } else {
-        echo "$item removido da lista!\n";
-    }
+	$arrayMerge = array_merge($listaCompras, $arrayListaDeCompras);
+
+	$arrayConvertido = serialize($arrayMerge);
+
+	$conteudo = file_put_contents("lista-compras.txt", $arrayConvertido);
+
+	if ($conteudo == false) {
+		return "Erro ao adicionar item no arquivo \n";
+	}
+
+	return "Item adicionado com sucesso \n";
+}
+
+function removerItem($indiceParaRemover) {
+	$resultado = handleFile();
+
+	
+
+	if (is_array($resultado)) {
+		$itemParaRemover = $resultado[$indiceParaRemover];
+
+		unset($itemParaRemover);
+
+   	 	$arquivoSalvo = file_put_contents("lista-compras.txt", serialize($resultado)); 
+
+    		if ($arquivoSalvo === false) {
+        		return "Falha ao remover $item da lista!\n";
+    		}
+	
+        	return ucfirst($itemParaRemover) .  " removido da lista!\n";
+	} else {
+		echo $resultado;
+	} 
 }
 
 function listarItens() {
-    $arquivo = file_get_contents("lista-de-compras.txt");
+	$resultado = handleFile();
 
-    $arrayListaDeCompras = unserialize($arquivo);
-
-    if ($arrayListaDeCompras === false) {
-        echo "Erro ao converter a lista de compras!\n";
-        return;
-    }
-
-    if (count($arrayListaDeCompras) == 0) {
-        echo "A lista está vazia!\n";
-        return;
-    }
-
-    echo "Lista de compras:\n";
-    echo "-----------------\n";
-    foreach ($arrayListaDeCompras as $item) {
-        echo '- ' . ucfirst($item) . PHP_EOL;
-    }
-    echo "-----------------\n";
-    echo "\n";
+	if (is_array($resultado)) {
+		foreach ($resultado as $indice => $item) {
+			echo $indice .' - '.$item . PHP_EOL;
+		}
+	}
+	
 }
 
-while (true) { 
-    echo "Escolha uma opção:\n";
-    echo "1. Adicionar item\n";
-    echo "2. Remover item\n";
-    echo "3. Listar itens\n";
-    echo "4. Consultar quantidade\n";
-    echo "5. Sair\n"; 
-    
-    $opcao = readline("Digite sua opção: ");
+function exibirQuantidade() {
+	$resultado = handleFile();
 
-    if ($opcao == 0 || $opcao > 5) {
-        echo "Opção inválida! Tente novamente.\n";
-    }
-
-    if ($opcao == 1) { 
-        $item = readline("Digite o nome do item: ");
-        adicionarItem($item);
-    } 
-
-    if ($opcao == 2) {
-        removerItem(); 
-    }
-
-    if ($opcao == 3){
-        listarItens();
-    }
-
-    if ($opcao == 4) {
-        $quantidade = count($listadeCompras); 
-        echo "Quantidade de itens na lista: $quantidade\n";
-    }
-
-    if ($opcao == 5) {
-        echo "Saindo... Até logo!\n";
-        exit;
-    } 
+	if (is_array($resultado)) {
+		echo count($resultado) . " itens na sua lista" . PHP_EOL;
+		listarItens();
+	} else {
+		echo $resultado;
+	}
 }
 
-?>
+while(true) {
+	// Exibe o menu para o usuário
+    	echo "Escolha uma opção:\n";
+    	echo "1. Adicionar item\n";
+    	echo "2. Remover item\n";
+    	echo "3. Listar itens\n";
+    	echo "4. Consultar quantidade\n";
+    	echo "5. Sair\n";
+
+	$opcao = readline("Digite uma opção: ");
+
+	echo PHP_EOL;
+	echo '------------------------------';
+	echo PHP_EOL;
+
+	if ($opcao == 0 || $opcao > 5) {
+		echo "Opção inválida, digite alguma do menu. \n";
+	}
+	
+	if ($opcao == 1) {
+		$item = readline("Digite o nome do item: ");
+		$mensagem = adicionarItem($item);
+		
+		echo $mensagem;
+	}
+
+	if ($opcao == 2) {
+		listarItens();
+		$itemParaRemover = readline("Digite o numero do item a ser removido: ");
+		$mensagem = removerItem($itemParaRemover);
+
+		echo $mensagem;
+	}
+
+	if ($opcao == 3) {
+		listarItens();
+	}
+
+	if ($opcao == 4) {
+		exibirQuantidade();
+	}
+
+	if ($opcao == 5) {
+		exit;
+	}
+}
