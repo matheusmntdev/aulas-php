b9b1ae3748e5fcb10fcf1d3a29d8a38385fa7909
Matheus Monteiro <matheusmnt.dev@gmail.com>
2025-01-31 16:59:35 -0300

    Update lista-compras-com-arquivos.php

--- lista-compras-com-arquivos.php
+++ lista-compras-com-arquivos.php
@@ -1,136 +1,138 @@
 <?php
+function handleFile()
+{
+    $arquivo = file_get_contents("lista-compras.txt");
 
-$listaCompras = [];
+    $arrayListaDeCompras = unserialize($arquivo);
 
-function handleFile() {
-	$arquivo = file_get_contents("lista-compras.txt");
+    if ($arrayListaDeCompras === false) {
+        return "Erro ao converter a lista de compras ou seu arquivo está vazio. \n \n";
+    }
 
-	$arrayListaDeCompras = unserialize($arquivo);
+    if (count($arrayListaDeCompras) == 0) {
+        return "A lista está vazia. \n \n";
+    }
 
-	if ($arrayListaDeCompras === false) {
-        	return "Erro ao converter a lista de compras";
-    	} 
-
-	if (count($arrayListaDeCompras) == 0) {
-        	return "A lista está vazia, não há itens para remover.\n";
-    	}
-
-	return $arrayListaDeCompras;
+    return $arrayListaDeCompras;
 }
 
-function adicionarItem($item) {
-	global $listaCompras;
+function adicionarItem($item)
+{
+    $arrayListaDeCompras = [];
 
-	array_push($listaCompras, $item);
+    if (file_exists("lista-compras.txt")) {
+        $arrayListaDeCompras = unserialize(file_get_contents("lista-compras.txt"));
+    } 
 
-	$arquivo = file_get_contents("lista-compras.txt");
+    $arrayListaDeCompras[] = $item;
 
-	$arrayListaDeCompras = unserialize($arquivo);
+    $arrayConvertido = serialize($arrayListaDeCompras);
 
-	if (!is_array($arrayListaDeCompras)) {
-		$arrayListaDeCompras = [];
-	}
+    $conteudo = file_put_contents("lista-compras.txt", $arrayConvertido);
 
-	$arrayMerge = array_merge($listaCompras, $arrayListaDeCompras);
+    if ($conteudo == false) {
+        return "Erro ao adicionar item no arquivo \n \n";
+    }
 
-	$arrayConvertido = serialize($arrayMerge);
-
-	$conteudo = file_put_contents("lista-compras.txt", $arrayConvertido);
-
-	if ($conteudo == false) {
-		return "Erro ao adicionar item no arquivo \n";
-	}
-
-	return "Item adicionado com sucesso \n";
+    return "Item '$item' adicionado com sucesso! \n \n";
 }
 
-function removerItem($indiceParaRemover) {
-	$resultado = handleFile();
+function removerItem($indiceParaRemover)
+{
+    $resultado = handleFile();
 
-	
+    if (is_array($resultado)) {
+        if (!key_exists($indiceParaRemover, $resultado)) {
+            return "Escolha um item que está na sua lista para poder remover. \n \n";
+        }
 
-	if (is_array($resultado)) {
-		$itemParaRemover = $resultado[$indiceParaRemover];
+        $itemRemovido = ucfirst($resultado[$indiceParaRemover]);
 
-		unset($itemParaRemover);
+        unset($resultado[$indiceParaRemover]);
 
-   	 	$arquivoSalvo = file_put_contents("lista-compras.txt", serialize($resultado)); 
+        $arrayOrganizado = array_values($resultado);
 
-    		if ($arquivoSalvo === false) {
-        		return "Falha ao remover $item da lista!\n";
-    		}
-	
-        	return ucfirst($itemParaRemover) .  " removido da lista!\n";
-	} else {
-		echo $resultado;
-	} 
+        $arquivoSalvo = file_put_contents("lista-compras.txt", serialize($arrayOrganizado));
+
+        if ($arquivoSalvo === false) {
+            return "Falha ao remover $itemRemovido da lista!\n \n";
+        }
+
+        return "$itemRemovido removido da lista!\n \n";
+    } else {
+        echo $resultado;
+    }
 }
 
-function listarItens() {
-	$resultado = handleFile();
+function listarItens()
+{
+    $resultado = handleFile();
 
-	if (is_array($resultado)) {
-		foreach ($resultado as $indice => $item) {
-			echo $indice .' - '.$item . PHP_EOL;
-		}
-	}
-	
+    echo "Sua lista de compras contém: \n \n";
+    if (is_array($resultado)) {
+        foreach ($resultado as $indice => $item) {
+            echo $indice . ' - ' . $item . PHP_EOL;
+        }
+        echo "-----------------------------\n";
+    } else {
+        echo $resultado;
+    }
 }
 
-function exibirQuantidade() {
-	$resultado = handleFile();
+function exibirQuantidade()
+{
+    $resultado = handleFile();
 
-	if (is_array($resultado)) {
-		echo count($resultado) . " itens na sua lista" . PHP_EOL;
-		listarItens();
-	} else {
-		echo $resultado;
-	}
+    if (is_array($resultado)) {
+        echo count($resultado) . " itens na sua lista" . PHP_EOL;
+        listarItens();
+    } else {
+        echo $resultado;
+    }
 }
 
-while(true) {
-	// Exibe o menu para o usuário
-    	echo "Escolha uma opção:\n";
-    	echo "1. Adicionar item\n";
-    	echo "2. Remover item\n";
-    	echo "3. Listar itens\n";
-    	echo "4. Consultar quantidade\n";
-    	echo "5. Sair\n";
+while (true) {
+    echo "Escolha uma opção:\n";
+    echo "1. Adicionar item\n";
+    echo "2. Remover item\n";
+    echo "3. Listar itens\n";
+    echo "4. Consultar quantidade\n";
+    echo "5. Sair\n";
 
-	$opcao = readline("Digite uma opção: ");
+    $opcao = readline("Digite uma opção: ");
 
-	echo PHP_EOL;
-	echo '------------------------------';
-	echo PHP_EOL;
+    echo PHP_EOL;
+    echo '------------------------------';
+    echo PHP_EOL;
 
-	if ($opcao == 0 || $opcao > 5) {
-		echo "Opção inválida, digite alguma do menu. \n";
-	}
-	
-	if ($opcao == 1) {
-		$item = readline("Digite o nome do item: ");
-		$mensagem = adicionarItem($item);
-		
-		echo $mensagem;
-	}
+    if ($opcao == 0 || $opcao > 5) {
+        echo "Opção inválida, digite alguma do menu. \n";
+    }
 
-	if ($opcao == 2) {
-		listarItens();
-		$itemParaRemover = readline("Digite o numero do item a ser removido: ");
-		$mensagem = removerItem($itemParaRemover);
+    if ($opcao == 1) {
+        $item = readline("Digite o nome do item: ");
+        $mensagem = adicionarItem($item);
 
-		echo $mensagem;
-	}
+        echo $mensagem;
+    }
 
-	if ($opcao == 3) {
-		listarItens();
-	}
+    if ($opcao == 2) {
+        listarItens();
+        $itemParaRemover = readline("Digite o numero do item a ser removido: ");
+        $mensagem = removerItem($itemParaRemover);
 
-	if ($opcao == 4) {
-		exibirQuantidade();
-	}
+        echo $mensagem;
+    }
 
-	if ($opcao == 5) {
-		exit;
-	}
+    if ($opcao == 3) {
+        listarItens();
+    }
+
+    if ($opcao == 4) {
+        exibirQuantidade();
+    }
+
+    if ($opcao == 5) {
+        exit;
+    }
 }
